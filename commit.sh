01849e41d7d7b5a633beaef0ea5d6be0c5ec087a
dantpro <dant@dant.pro>
2018-09-22 02:55:17 +0700

    commited on 180922-025517

--- commit.sh
+++ commit.sh
@@ -2,19 +2,19 @@
 
 # git push ironman repo to github
 
-dated=`date +%Y%m%d-%H%M%S`
+date=`date +%y%m%d-%H%M%S`
 
 echo run git add
 git add .
 
-echo run git push on ${dated}
+echo run git push on ${date}
 
 
 for i in `git status | grep deleted | awk '{print $2}'`; do git rm $i; done
 
 # git add -u .
 #
-git commit -m 'commited on '${dated}
+git commit -m 'commited on '${date}
 
 #git commit -m "$dated $(curl -s http://whatthecommit.com/index.txt)"
 git push -u origin master
