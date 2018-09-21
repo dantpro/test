f0cebb2f26b2c8b91fa9c1b32db02511c06a7e98
dantpro <dant@dant.pro>
2018-09-22 03:04:27 +0700

    commited on 180922-030427

--- commit.sh
+++ commit.sh
@@ -2,15 +2,15 @@
 
 date=`date +%y%m%d-%H%M%S`
 
-echo "Run git add...\n"
+echo "Run git add:\n"
 git add .
 
-echo "\nRun git commit on ${date}...\n"
+echo "\nRun git commit on ${date}:\n"
 for i in `git status | grep deleted | awk '{print $2}'`; do git rm $i; done
 
 git commit -m 'commited on '${date}
 #git commit -m "$date $(curl -s http://whatthecommit.com/index.txt)"
 
-echo "\nRun git push...\n"
+echo "\nRun git push:\n"
 git push -u origin master
 
